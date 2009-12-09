24fce9b194d34eb45a622b69f6f9d9bdbc2f4153
Rune Peter Bjornstad <runepeter@gmail.com>
2009-12-09 21:18:00 +0100

    - Updated SCM in POM to require GitHub as the default scm provider - Optimies imports.

--- fundament-core/src/main/java/no/jforce/fundament/pattern/Locator.java
+++ fundament-core/src/main/java/no/jforce/fundament/pattern/Locator.java
@@ -1,9 +1,8 @@
 package no.jforce.fundament.pattern;
 
-import no.jforce.fundament.service.Service;
-
 /**
  * Marker-interface for <tt>Locator</tt> implementations.
+ *
  * @author <a href="mailto:runepeter@gmail.com">Rune Peter Bj&oslash;rnstad</a>
  */
 public interface Locator {
